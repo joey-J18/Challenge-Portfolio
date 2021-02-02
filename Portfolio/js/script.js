c23c4eb5b4a37989ca9dcfd39eeebbc6fe5b4d38
Joey Janmaat <Joey.janmaat@student.rocva.nl>
2021-02-02 12:43:09 +0100

    js aangemaakt

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,1 +1,21 @@
-alert ("Welkom op mijn website!");
+var bezoeker = {
+    voornaam : prompt("Wat is je voornaam"),
+   
+    leeftijd : prompt("wat is je leeftijd"),
+};
+
+
+
+
+document.querySelector("#Voornaam").innerHTML = (bezoeker.voornaam)
+
+document.querySelector("#Leeftijd").innerHTML = (bezoeker.leeftijd)
+
+
+
+if (bezoeker.leeftijd == 19){
+    document.body.style.backgroundColor = "green";
+}
+if (bezoeker.leeftijd == null){
+    document.body.style.backgroundColor = "purple";
+};
